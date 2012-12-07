47aea103eccc6da839eae7b2adf898077c75a6ef
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-12-07 16:04:04 +0100

    Model support building from dictionary, 1st version of sync script

--- assignment/sync.py
+++ assignment/sync.py
@@ -2,5 +2,27 @@
 import os
 import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assignment.settings")
-from users.models import *
+import json
 
+from users.models import *
+from django.conf import settings
+
+
+from remote import Remote
+
+remote_db = Remote(settings.REMOTE_API_USERNAME,settings.REMOTE_API_PASSWORD,settings.REMOTE_API_BASE_URL)
+
+def sync():
+    r='v1/account_lead/'
+
+    b=remote_db.request(r)
+    b=json.loads(b)
+
+    for i in b['objects']:
+        if len(User.objects.filter(resource_uri=i['resource_uri']))==0:
+            #User does not exist locally
+            User.create_from_dic(i)
+
+            
+if __name__ == "__main__":
+    sync()
--- assignment/users/models.py
+++ assignment/users/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 
 class MailingList(models.Model):
-    name = models.CharField(max_length=200,default='')
+    name = models.CharField(max_length=200,default='',unique=True)
     resource_uri = models.CharField(max_length=200,default='')
+    @staticmethod
+    def return_from_dic(d):
+        '''Creates the object from a dictionary and returns it.
+        If the object already exists, returns it without creating anything.'''
+        search = MailingList.objects.filter(name=d['name'])
+        
+        if len(search) == 1:
+            return search[0]
+        newml = MailingList(**d)
+        newml.save()
+        return newml
+        
 class Referral(models.Model):
     name = models.CharField(max_length=200,default='')
     resource_uri = models.CharField(max_length=200,default='')
-    
+    @staticmethod
+    def return_from_dic(d):
+        '''Creates the object from a dictionary and returns it.'''
+        search = Referral.objects.filter(**d)
+        
+        if len(search) == 1:
+            return search[0]
+        newr = Referral(**d)
+        newr.save()
+        return newr
+        pass
 class User(models.Model):
     birth_date = models.DateField(null=True)
     city = models.CharField(max_length=200,default='')
@@ -19,7 +41,7 @@ class User(models.Model):
     phone = models.CharField(max_length=60,default='')
     street_number = models.CharField(max_length=10,default='')
     mailing_lists = models.ManyToManyField(MailingList)
-    resource_uri = models.CharField(max_length=200,editable=False,unique=True) #remote url to the resource
+    resource_uri = models.CharField(max_length=200,editable=False,unique=True,primary_key=True) #remote url to the resource
     tr_input_method = models.CharField(max_length=200,default='')
     tr_ip_address = models.CharField(max_length=45,null=True) #TODO expand charfield model to represent an ip address
     tr_language = models.CharField(max_length=10,default='')
@@ -30,3 +52,17 @@ class User(models.Model):
     #TODO filtering = u'filtering': {u'email': 1, u'first_name': 1, u'last_name': 1}}
     zipcode = models.CharField(max_length=10)
     
+    @staticmethod
+    def create_from_dic(d):
+        '''Creates the object from a dictionary and returns it.'''
+        referral = Referral.return_from_dic(d['tr_referral'])
+        del(d['tr_referral'])
+        ml = map(MailingList.return_from_dic, d['mailing_lists'])
+        del(d['mailing_lists'])
+        
+        new_user = User(**d)
+        new_user.tr_referral = referral
+        map(new_user.mailing_lists.add,ml)
+        new_user.save()
+        
+        pass
