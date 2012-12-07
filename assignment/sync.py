b2a5c2636905f56e20aeeb7c0b7a38c551da62e3
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-12-07 16:17:43 +0100

    Can remove remotely deleted users

--- assignment/sync.py
+++ assignment/sync.py
@@ -17,12 +17,23 @@ def sync():
 
     b=remote_db.request(r)
     b=json.loads(b)
+    
+    uri_set = set()
 
+    #Add remote users locally
     for i in b['objects']:
+        uri_set.add(i['resource_uri'])
         if len(User.objects.filter(resource_uri=i['resource_uri']))==0:
             #User does not exist locally
             User.create_from_dic(i)
-
+    
+    #Remove remote users locally
+    to_delete = []
+    for i in User.objects.iterator():
+        if i.resource_uri not in uri_set:
+            to_delete.append(i)
+    print "Removing %d local users who don't exist remotely" % len(to_delete)
+    map(User.delete,to_delete)
             
 if __name__ == "__main__":
     sync()
