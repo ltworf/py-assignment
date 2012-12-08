e0fdf8f55955c5c704d517bab4ac226849c18e41
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-12-08 16:26:31 +0100

    simple authentication

--- assignment/users/api.py
+++ assignment/users/api.py
@@ -5,33 +5,22 @@ from users.models import User
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
 
+from django.conf import settings
+
 
 class MyAuthentication(Authentication):
     def is_authenticated(self, request, **kwargs):
         return True
-        if 'salvo' in request.user.username:
-          return True
 
-        return False
-
-    # Optional but recommended
     def get_identifier(self, request):
         return request.user.username
 
 class MyAuthorization(Authorization):
     def is_authorized(self, request, object=None):
+        if not (request.GET['username']==settings.PUBLIC_API_USERNAME and
+           request.GET['api_key'] == settings.PUBLIC_API_PASSWORD):
+               raise Exception('Unauthorized')
         return True
-        if request.user.date_joined.year == 2010:
-            return True
-        else:
-            return False
-
-    # Optional but useful for advanced limiting, such as per user.
-    #def apply_limits(self, request, object_list):
-    #    if request and hasattr(request, 'user'):
-    #        return object_list.filter(author__username=request.user.username)
-
-    #    return object_list.none()
 
 
 class UserResource(ModelResource):
@@ -39,5 +28,6 @@ class UserResource(ModelResource):
         queryset = User.objects.all()
         resource_name = 'user'
         
+        #TODO yes this is not very evolved but i don't have a user database now
         authentication = MyAuthentication()
         authorization = MyAuthorization()
